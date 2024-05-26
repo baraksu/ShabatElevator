59fa02afd6b61c887868d38ff75163c3d8ce9e70
arigotd <arigot30@gmail.com>
2024-05-26 07:22:17 -0700

    Update shabat elevator.asm

--- shabat elevator.asm
+++ shabat elevator.asm
@@ -21,9 +21,246 @@ x_2 dw 100
 y_2 dw 20
 first_floor dw 100
 next_floor dw ? 
+                
+                
+x_center dw 20
+y_center dw 130
+y_value dw 0
+x_value dw 10 
+decision dw ?
+color_o db 0fh 
+y_stickman dw 150
+x_stickman dw 30
+h dw 30                
+                
+                
+
+.CODE 
+proc circle
+
  
 
-.CODE
+
+
+     mov bx, x_value
+     mov ax,2
+     mul bx
+     mov bx,3
+     sub bx,ax 
+     mov decision,bx
+     mov al,color_o
+     mov ah,0ch
+     drawcircle:
+     mov al,color_o
+     mov ah,0ch 
+     mov cx, x_value
+     add cx, x_center 
+     mov dx, y_value
+     add dx, y_center
+     int 10h
+     mov cx, x_value 
+     neg cx
+     add cx, x_center 
+     int 10h
+     mov cx, y_value     
+     add cx, x_center 
+     mov dx, x_value
+     add dx, y_center
+     int 10h 
+     mov cx, y_value 
+     neg cx
+     add cx, x_center 
+     int 10h
+     mov cx, x_value 
+     add cx, x_center
+     mov dx, y_value
+     neg dx
+     add dx, y_center
+     int 10h
+
+; 
+
+ mov cx, x_value 
+ neg cx
+
+ add cx, x_center 
+
+ int 10h
+
+
+
+ mov cx, y_value 
+
+ add cx, x_center 
+
+ mov dx, x_value
+
+ neg dx
+
+ add dx, y_center
+
+ int 10h
+
+; 
+
+ mov cx, y_value 
+
+ neg cx
+
+ add cx, x_center 
+
+ int 10h
+
+ 
+
+condition1:
+
+ cmp decision,0
+
+ jg condition2      
+
+ mov cx, y_value
+
+ mov ax, 2
+
+ imul cx 
+
+ add ax, 3 
+
+ mov bx, 2
+
+ mul bx  
+
+ add decision, ax
+
+ mov bx, y_value
+
+ mov dx, x_value
+
+ 
+
+  
+
+ inc y_value
+
+ jmp drawcircle
+
+
+
+condition2:
+
+ mov cx, y_value 
+
+ mov ax,2
+
+ mul cx 
+
+ mov bx,ax
+
+ mov cx, x_value
+
+ mov ax, -2
+
+ imul cx 
+
+ add bx,ax
+
+ add bx,5
+
+ mov ax,2
+
+ imul bx        
+
+ add decision,ax
+
+ mov bx, y_value
+
+ mov dx, x_value
+
+ cmp bx, dx
+
+ ja donedrawing
+
+ dec x_value    
+
+ inc y_value
+
+ jmp drawcircle
+ 
+donedrawing:
+mov cx,x_stickman
+mov dx,y_stickman
+add dx,h
+mov al,0fh
+mov ah,0ch
+next3:
+int 10h
+dec dx
+cmp dx,y_stickman
+jne next3
+;lf hand
+mov bx,10
+mov cx,30
+mov dx,155
+mov ah,0ch
+mov al,0fh
+lfhand:
+int 10h
+ inc cx  
+ inc dx 
+ dec bx 
+ cmp bx,0
+ jne lfhand 
+ ;r_hand
+    mov bx,10
+    mov cx,30
+    mov dx,155
+    mov ah,0ch
+    mov al,0fh
+rhandloop:
+int 10h
+dec cx ; increase x values 
+ inc dx ; decrease y values
+ dec bx ; decrease length
+ cmp bx,0
+ jne rhandloop
+ 
+ ;lf lag
+mov bx,10
+mov cx,30
+mov dx,180
+mov ah,0ch
+mov al,0fh
+lfleg:
+int 10h
+ inc cx  
+ inc dx 
+ dec bx 
+ cmp bx,0
+ jne lfleg 
+ ;r lag
+    mov bx,10
+    mov cx,30
+    mov dx,180
+    mov ah,0ch
+    mov al,0fh
+rlegloop:
+int 10h
+dec cx ; increase x values 
+ inc dx ; decrease y values
+ dec bx ; decrease length
+ cmp bx,0
+ jne rlegloop
+ 
+ 
+ 
+ 
+  
+
+ret
+endp circle+
+
+
 
 start: 
     mov ax, @data
@@ -82,7 +319,7 @@ mov floor_size, ax
 
 
  
- mov ax,19h
+ mov ax,13h
  int 10h 
  
  loop:
@@ -96,7 +333,7 @@ mov floor_size, ax
 	mov ah, 0ch
 	int 10h
 	inc y_2
-	cmp y_2,199
+	cmp y_2,180
 	jb draw_LefteLine  
 	
 
@@ -148,9 +385,52 @@ mov floor_size, ax
 	int 10h  
 	inc first_floor
 	cmp first_floor, 200
-	jb draw_Floor 
+	jb draw_Floor   
 	
-	    
+	
+	  proc new_floor
+	pusha            
+	xor bh, bh  ; bh = 0
+	mov cx, [first_floor]
+	sub dx, floor_size
+	
+	mov ax, [color] 
+	mov ah, 0ch
+	int 10h  
+	dec first_floor
+	cmp first_floor, 100
+	ja new_Floor 
+	
+	
+	
+	int 10h 
+
+ mov x_center,30
+
+ mov y_center,140
+
+ mov y_value,0
+
+ mov x_value,10
+
+ call circle
+	
+	
+	
+	
+	
+     
+   
+   
+   
+   
+exit:
+			
+
+  mov AH,4CH
+  int 21h
+
+END start    
 	
 	
 	
